e7d48351cf0f362353813e6f75c4b45487a9938a
Sadman37874 <md.supto1@gmail.com>
2021-02-18 23:00:42 +0600

    MID_LAB_04

---
+++ MID_LAB_TASK_PHP_04/page2.php
@@ -0,0 +1,10 @@
+<?php
+
+
+ $vat = 15;
+ $price = 20000;
+ $vatammount = ($price / 100) * $vat;
+
+echo "Total vat = $vatammount taka" ;
+
+?>
---
+++ MID_LAB_TASK_PHP_04/page3.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = 2000;
+
+
+if ($a%2 == 0) 
+    {
+	echo "$a is even!";
+    }
+	
+else{
+	echo "$a is odd!";
+    }
+	
+?>
