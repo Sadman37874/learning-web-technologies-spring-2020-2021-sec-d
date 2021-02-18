6d936e07fbd56fa4f3b7b4e1b5870cee0b704c7e
Sadman37874 <md.supto1@gmail.com>
2021-02-18 23:02:36 +0600

    MID_LAB_TASK_PHP_05

---
+++ MID_LAB_TASK_PHP_05/Task_6/TASK_B/Blood group.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HTML-FORM</title>
+</head>
+<body>
+
+	<form method="" action="">
+		<fieldset>
+
+
+			<table>
+
+				<tr>
+					<td>Blood Group:</td>
+					<td>
+						<select name="BloodGroup">
+							<option value="A+"></option>
+							<option value="B+"></option>
+							<option value="O+" selected>A+</option>
+						</select>
+					</td>
+				</tr>
+
+					<td>
+
+						<input type="submit" name="button" value="Submit">
+
+					</td>
+
+			</table>
+
+		</fieldset>
+
+	</form>
+</body>
+</html>
---
+++ MID_LAB_TASK_PHP_05/Task_6/TASK_C/Blood group.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HTML-FORM</title>
+</head>
+<body>
+
+	<form method="" action="">
+		<fieldset>
+
+
+			<table>
+
+				<tr>
+					<td>Blood group:</td>
+					<td>
+						
+						<select name="BloodGroup">
+							<option value="A+">A+</option>
+							<option value="B+">B+</option>
+							<option value="O+" selected>A+</option>
+						</select>
+					</td>
+				</tr>
+
+					<td>
+
+						<input type="submit" name="button" value="Submit">
+
+					</td>
+
+			</table>
+
+		</fieldset>
+
+	</form>
+</body>
+</html>
